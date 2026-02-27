17fa3d780b09456e4a64787264dc41b95abf900b
Marco Mendes <corelio@gmail.com>
2026-02-27 14:39:59 -0300

    feat: adicionar Pedidos e PedidoItens ao AppDbContext com migration

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ProdutosAPI.Features.Pedidos.Domain;
 using ProdutosAPI.Models;
 
 namespace ProdutosAPI.Data;
@@ -16,6 +17,8 @@ public class AppDbContext : DbContext
 
     // DbSets
     public DbSet<Produto> Produtos => Set<Produto>();
+    public DbSet<Pedido> Pedidos => Set<Pedido>();
+    public DbSet<PedidoItem> PedidoItens => Set<PedidoItem>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -68,6 +71,61 @@ public class AppDbContext : DbContext
             entity.Property(p => p.ContatoEmail).UsePropertyAccessMode(PropertyAccessMode.Property);
             entity.Property(p => p.DataCriacao).UsePropertyAccessMode(PropertyAccessMode.Property);
             entity.Property(p => p.DataAtualizacao).UsePropertyAccessMode(PropertyAccessMode.Property);
+        });
+
+        modelBuilder.Entity<Pedido>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+
+            entity.Property(p => p.Status)
+                .HasConversion<string>()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(p => p.Total)
+                .HasPrecision(10, 2)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(p => p.CriadoEm)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(p => p.ConfirmadoEm)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(p => p.CanceladoEm)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(p => p.MotivoCancelamento)
+                .HasMaxLength(500)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.HasMany(p => p.Itens)
+                .WithOne()
+                .HasForeignKey(i => i.PedidoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.Navigation(p => p.Itens).UsePropertyAccessMode(PropertyAccessMode.Field);
+        });
+
+        modelBuilder.Entity<PedidoItem>(entity =>
+        {
+            entity.HasKey(i => i.Id);
+
+            entity.Property(i => i.NomeProduto)
+                .IsRequired()
+                .HasMaxLength(100)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(i => i.PrecoUnitario)
+                .HasPrecision(10, 2)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(i => i.Quantidade)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Property(i => i.ProdutoId)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            entity.Ignore(i => i.Subtotal);
         });
     }
 }
--- src/Data/Migrations/20260227173933_AddPedidos.Designer.cs
+++ src/Data/Migrations/20260227173933_AddPedidos.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProdutosAPI.Data;
 
@@ -10,9 +11,11 @@ using ProdutosAPI.Data;
 namespace ProdutosAPI.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260227173933_AddPedidos")]
+    partial class AddPedidos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
